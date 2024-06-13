d289d7937384328be31bca4dc9323d48c0354998
Gustavo de Oliveira Neves <chips_gustavo2@outlook.com>
2024-06-13 03:39:35 -0300

    Ajusta o res.json

--- ws/src/middlewares/authenticate.js
+++ ws/src/middlewares/authenticate.js
@@ -7,25 +7,25 @@ module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if(!authHeader){
-        return res.status(401).json({error:true, message:"Sem token"});
+        return res.json({error:true, message:"Sem token"});
     }
 
     const parts = authHeader.split(" ")
 
     if(parts.length !=2){
-        return res.status(401).json({error:true, message:"Invalid"});
+        return res.json({error:true, message:"Invalid"});
     }
 
     const [ scheme, token] = parts;
 
     if(scheme.indexOf("Bearer") !== 0){
-        return res.status(401).json({
+        return res.json({
             error:true, message:"token malformeted"
         })
     }
     jwt.verify(token, authConfig.secret, (err, decoded) =>{
         if(err){
-            return res.status(401).json({error:true, message:"Invalid token/expired"});
+            return res.json({error:true, message:"Invalid token/expired"});
         }
         req.userLogged = decoded;
         return next();
