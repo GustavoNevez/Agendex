d2edb74833c49b188a315bff6817f37dea859161
Gustavo de Oliveira Neves <chips_gustavo2@outlook.com>
2025-05-14 21:07:51 -0300

    Font Config

--- web/src/index.js
+++ web/src/index.js
@@ -6,6 +6,14 @@ import store from './store';
 import { AuthProvider } from './context/auth';
 import { RouteProvider } from './context/routeSide';
 import { BrowserRouter as Router } from 'react-router-dom';
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
+
+import '@fontsource/urbanist/400.css'
+import '@fontsource/urbanist/700.css'
+
+import '@fontsource/gantari/400.css'
+import '@fontsource/gantari/700.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
