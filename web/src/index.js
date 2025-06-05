4bd4f1477e4b413817db8bcb0474eb231953a9c1
GustavoNeves-AI <neves_gustavo@outlook.com>
2025-06-05 00:08:54 -0300

    feat: add LandingPage component with features, pricing, and contact sections

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store/store_saga";
 import { AuthProvider } from "./context/auth_provider";
 import { RouteProvider } from "./context/router_provider";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
 
@@ -14,6 +14,7 @@ import "@fontsource/urbanist/700.css";
 
 import "@fontsource/gantari/400.css";
 import "@fontsource/gantari/700.css";
+import LandingPage from "./landing/LandingPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -21,7 +22,10 @@ root.render(
     <AuthProvider>
       <Router>
         <RouteProvider>
-          <Routers />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/" component={Routers} />
+          </Switch>
         </RouteProvider>
       </Router>
     </AuthProvider>
